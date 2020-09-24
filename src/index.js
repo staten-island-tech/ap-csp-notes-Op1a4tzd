a48c71ea0fbec542e9fd214397e1a168fb3a31e7
Junshen Gao <57498746+Op1a4tzd@users.noreply.github.com>
2020-09-24 11:51:56 -0400

    a

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const tipForm = document.querySelector("#tip-form");
-const billAmount = document.querySelector("bill-amount");
-console.log(billAmount);
+const tipForm = document.getElementById("tip-form");
+
+
+tipForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const billAmount = Number(document.querySelector("#bill-amount").value);
+    const tipPercentage = Number(document.querySelector("#tip-amount").value);
+    const tipAmount = document.querySelector("#final-tip");
+    tipAmount.innerHTML = "50";
+});
